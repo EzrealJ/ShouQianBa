1c0d0acebc9604284c0f53ca66b85c78858c886f
Ezreal <memory_of_you@hotmail.com>
2019-01-11 02:39:03 +0800

    add CentStringConverter

---
+++ Ezreal.SDK.ShouQianBa.Test/DataConvert/CentStringConverterTest.cs
@@ -0,0 +1,48 @@
+﻿using Ezreal.SDK.ShouQianBa.DataConvert;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ezreal.SDK.ShouQianBa.Test.DataConvert
+{
+    public class CentStringConverterTest
+    {
+        public class TestClass
+        {
+            [JsonConverter(typeof(CentStringConverter))]
+            public float MyProperty { get; set; }
+            [JsonConverter(typeof(CentStringConverter))]
+            public double MyProperty1 { get; set; }
+            [JsonConverter(typeof(CentStringConverter))]
+            public decimal MyProperty2 { get; set; }
+        }
+        [Fact]
+        public void SerializeObject()
+        {
+            TestClass testClass = new TestClass()
+            {
+                MyProperty = 12.3f,
+                MyProperty1 = 11.2d,
+                MyProperty2 = 52.3m
+            };
+
+            string str = JsonConvert.SerializeObject(testClass);
+            Assert.Contains("\"1230\"",str);
+            Assert.Contains("\"1120\"", str);
+            Assert.Contains("\"5230\"", str);
+        }
+        [Fact]
+        public void DeserializeObject()
+        {
+            string str = "{\"MyProperty\":\"1230\",\"MyProperty1\":\"1120\",\"MyProperty2\":\"5230\"}";
+            TestClass testClass = JsonConvert.DeserializeObject<TestClass>(str);
+            Assert.True(testClass.MyProperty == 12.3f);
+            Assert.True(testClass.MyProperty1 == 11.2d);
+            Assert.True(testClass.MyProperty2 == 52.3m);
+        }
+    }
+
+
+}
--- Ezreal.SDK.ShouQianBa/DataConvert/CentStringConverter.cs
+++ Ezreal.SDK.ShouQianBa/DataConvert/CentStringConverter.cs
@@ -18,20 +18,23 @@ namespace Ezreal.SDK.ShouQianBa.DataConvert
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            if (!allowTypes.Contains(objectType))
+            {
+                throw new TypeAccessException(objectType.ToString());
+            }
+            return Convert.ChangeType(decimal.Parse(serializer.Deserialize(reader, objectType).ToString())/100,objectType);
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            //Type fromType = value.GetType();
-            //if (!allowTypes.Contains(fromType))
-            //{
-            //    throw new TypeAccessException(fromType.ToString());
-            //}
-            //if(value is float)
-            //{
-                
-            //}
+            Type fromType = value.GetType();
+            if (!allowTypes.Contains(fromType))
+            {
+                throw new TypeAccessException(fromType.ToString());
+            }
+
+            serializer.Serialize(writer, ((int)(Math.Round(decimal.Parse(value.ToString()), 2) * 100)).ToString());
+
         }
     }
 }
