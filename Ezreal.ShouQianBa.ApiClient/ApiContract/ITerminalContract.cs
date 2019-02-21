be431f827b191699dc280228859389666304a050
Ezreal <memory_of_you@hotmail.com>
2019-02-21 15:58:00 +0800

    Update ITerminalContract.cs

--- Ezreal.ShouQianBa.ApiClient/ApiContract/ITerminalContract.cs
+++ Ezreal.ShouQianBa.ApiClient/ApiContract/ITerminalContract.cs
@@ -56,7 +56,6 @@ namespace Ezreal.ShouQianBa.ApiClient.ApiContract
         ///// <param name="terminalSignProvider"></param>
         ///// <param name="mulitpartFile"></param>
         ///// <returns></returns>
-        //[Obsolete("根据文档尚不能明确正确的签名/上传方式,作者认为其描述是片面的，不合理的;文档要求使用zip格式压缩,压缩后得到的文件,如何签名? base64?亦或是直接MD5 hash Hex?仍需要application/json的Header?")]
         //[Timeout(5 * 1000)]
         //[HttpPost("terminal/uploadLog")]
         //[JsonReturn]
