82aa300fd28209a142402f3a087792e182030220
Ezreal <memory_of_you@hotmail.com>
2020-06-15 16:47:36 +0800

    更新收钱吧提供的高仿域名

--- Ezreal.ShouQianBa.ApiClient/ShouQianBaGlobalConfig.cs
+++ Ezreal.ShouQianBa.ApiClient/ShouQianBaGlobalConfig.cs
@@ -16,7 +16,7 @@ namespace Ezreal.ShouQianBa.ApiClient
         /// <summary>
         /// 生产环境Uri
         /// </summary>
-        public string ProductionEnvironmentApiUri { get; set; } = "https://api-vendor.shouqianba.com/";
+        public string ProductionEnvironmentApiUri { get; set; } = "https://vsi-api.shouqianba.com";
         /// <summary>
         /// 沙箱环境Uri
         /// </summary>
